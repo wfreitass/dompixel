260c5a973691dc6ad843b9c7d4527e7c8d3b25f2
Wiltter Freitas <wiltterfreitasw@gmail.com>
2023-07-23 14:47:24 -0300

    bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <title>Document</title>
+
 </head>
 
 <body>
+    <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+    </div>
     <form method="POST" action="{{ route('product.storeProducts') }}">
         @csrf
         <input type="text" placeholder="nome" name="name">
